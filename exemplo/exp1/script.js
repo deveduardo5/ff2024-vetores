fb59e00dd64fb537b339f78c29b8372f57275dbb
deveduardo5 <eduardo.asouza23@senacsp.edu.br>
2024-10-09 22:15:13 -0300

    feat: Exemplo 4 inserido

--- exemplo/exp1/script.js
+++ exemplo/exp1/script.js
@@ -1,33 +1,34 @@
-// Crie um algoritmo que um vetor de 10 letras, e diga quantas consoantes foram lidas e mostre essas cosoantes.
+/*
+Crie um algoritmo que leia o salário bruto de 5 pessoas, 
+e ao final, mostre o salário líquido de todos, 
+considerando 12% de INSS a descontar.
+*/
+function lerSalarioBruto() {
+    let salBruto = [];
+    for (let i = 0; i < 5; i++) {
+        let salariobruto = prompt(`Informe o ${i + 1}º Salário Bruto`);
+        if (isNaN(salariobruto) || salariobruto.trim() == '') {
+            alert("Salário Bruto Inválido!");
+            salariobruto = prompt(`Informe o ${i + 1}º Salário Bruto novamente:`);
+        }
+        salariobruto = parseFloat(salariobruto);
 
-let letras = [];
-for (let i = 0; i < 10; i++) {
-    let letra = prompt(`Digite a ${i + 1} ª letra:`);
-    while (!isNaN(letra) || letra.trim() == '' || letra.length !== 1) {
-        alert("Letra inválida!");
-        letra = prompt(`Digite a ${i + 1} ª letra novamente:`);
+        salBruto.push(salariobruto);
     }
-    letras.push(letra);
+    return salBruto;
 }
 
-let msg = "";
-let quantidade = 0;
-for (let j = 0; j < letras.length; j++) {
-    let letraLida = letras[j].toUpperCase();
-    let vgs = ["A", "E", "I", "O", "U"];
-    if (!(vgs[0] === letraLida) &&
-        !(vgs[1] === letraLida) &&
-        !(vgs[2] === letraLida) &&
-        !(vgs[3] === letraLida) &&
-        !(vgs[4] === letraLida)) {
-        msg += letras[j] + " ";
-        quantidade++
+function calcularLiquido(lista) {
+    let msg = "";
+    for (let i = 0; i < lista.length; i++) {
+        msg += `O ${i+1}º salario líquido sem o INSS é R$ ${(lista[i] * 0.88).toFixed(2)} \n`;
     }
+    return msg;
 }
-alert(`A quantidade de consoantes é ${quantidade} e as consoantes são elas: ${msg}`);
 
-
-
+let salarios = lerSalarioBruto();
+let mensagem = calcularLiquido(salarios);
+alert(mensagem);
 
 
 
